8dc1633d2eaed49c438319983d334033f35919af
Kamenskiy Andrey <87336070+LeDron12@users.noreply.github.com>
2021-11-10 02:31:06 +0300

    Delete python_encryption directory

--- python_encryption/constants.py
+++
@@ -1,7 +0,0 @@
-class Constants:
-    keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-    path1 = 'tests1.txt'
-    path2 = 'tests2.txt'
-    path3 = 'tests3.txt'
